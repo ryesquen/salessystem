99cc0db3b43a84c500847e40b5b42bce87869f57
Renzo Yesquén <ryesquen@gmail.com>
2023-05-02 16:39:03 -0500

    ya

---
+++ SalesSystem.Domain/Entities/Menu.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SalesSystem.Domain.Entities
+{
+    public partial class Menu
+    {
+        public Menu()
+        {
+            MenuRoles = new HashSet<MenuRol>();
+        }
+
+        public int IdMenu { get; set; }
+        public string? Nombre { get; set; }
+        public string? Icono { get; set; }
+        public string? Url { get; set; }
+
+        public virtual ICollection<MenuRol> MenuRoles { get; set; }
+    }
+}
---
+++ SalesSystem.Domain/Entities/Rol.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SalesSystem.Domain.Entities
+{
+    public partial class Rol
+    {
+        public Rol()
+        {
+            MenuRoles = new HashSet<MenuRol>();
+            Usuarios = new HashSet<Usuario>();
+        }
+
+        public int IdRol { get; set; }
+        public string? Nombre { get; set; }
+        public DateTime? FechaRegistro { get; set; }
+
+        public virtual ICollection<MenuRol> MenuRoles { get; set; }
+        public virtual ICollection<Usuario> Usuarios { get; set; }
+    }
+}
