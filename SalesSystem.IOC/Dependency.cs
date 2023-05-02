be96d8a8f048bed028cc3c64fccccc60a800542e
Renzo Yesquén <ryesquen@gmail.com>
2023-05-02 17:16:00 -0500

    borrar

---
+++ SalesSystem.IOC/Dependency.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SalesSystem.Persistence.Contexts;
+
+namespace SalesSystem.IOC
+{
+    public static class Dependency
+    {
+        public static void InjectionDependencies(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<DbVentaContext>(
+                options =>
+                {
+                    options.UseSqlServer(configuration.GetConnectionString("cn"));
+                });
+        }
+    }
+}
